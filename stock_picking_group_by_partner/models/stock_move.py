74059e21d92fcd31c65be75983b83b5f3b78d809
JhoneM <jhone0901@gmail.com>
2021-01-11 15:40:03 -0300

    [FIX][stock_picking_group_by_partner] Incidence nrb.s/: ODOO-651:  - Fix expected singleton error, the module could not handle packages.

--- stock_picking_group_by_partner/models/stock_move.py
+++ stock_picking_group_by_partner/models/stock_move.py
@@ -32,29 +32,28 @@ class StockMove(models.Model):
             Picking = self.env['stock.picking']
             grouped_moves = groupby(sorted(self, key=lambda m: [f.id for f in m._key_assign_picking()]), key=lambda m: [m._key_assign_picking()])
             for group, moves in grouped_moves:
-                # import wdb
-                # wdb.set_trace()
                 moves = self.env['stock.move'].concat(*list(moves))
                 new_picking = False
                 # Could pass the arguments contained in group but they are the same
                 # for each move that why moves[0] is acceptable
                 picking = moves[0]._search_picking_for_assignation()
-                if picking:
-                    if any(picking.partner_id.id != m.partner_id.id or
-                            picking.origin != m.origin for m in moves):
-                        # If a picking is found, we'll append `move` to its move list and thus its
-                        # `partner_id` and `ref` field will refer to multiple records. In this
-                        # case, we chose to  wipe them.
-                        picking.with_context(picking_no_overwrite_partner_origin=1).write({
-                            'partner_id': False,
-                            'origin': False,
-                        })
-                else:
-                    new_picking = True
-                    picking = Picking.create(moves[0]._get_new_picking_values())
+                for m in moves:
+                    if picking:
+                        if any(picking.partner_id.id != m.partner_id.id or
+                                picking.origin != m.origin for m in moves):
+                            # If a picking is found, we'll append `move` to its move list and thus its
+                            # `partner_id` and `ref` field will refer to multiple records. In this
+                            # case, we chose to  wipe them.
+                            picking.with_context(picking_no_overwrite_partner_origin=1).write({
+                                'partner_id': False,
+                                'origin': False,
+                            })
+                    else:
+                        new_picking = True
+                        picking = Picking.create(m._get_new_picking_values())
 
-                moves[0].write({'picking_id': picking.id})
-                moves[0]._assign_picking_post_process(new=new_picking)
+                    m.write({'picking_id': picking.id})
+                    m._assign_picking_post_process(new=new_picking)
             return True
 
 
