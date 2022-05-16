2f695a3518a12fea40ac2a653405add67984cd40
Fede Gregori <jfedericogregori@gmail.com>
2022-05-16 20:11:22 -0300

    Initial commit

---
+++ product_print_tag/__manifest__.py
@@ -0,0 +1,19 @@
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    "name": "Product Print Tag",
+    "summary": """
+        This module adds a tag format for products.
+    """,
+    "author": "Calyx Servicios S.A.",
+    "maintainers": ["PerezGabriela"],
+    "website": "http://odoo.calyx-cloud.com.ar/",
+    "license": "AGPL-3",
+    "category": "Stock",
+    "version": "13.0.1.0.0",
+    "depends": [
+        "stock"
+    ],
+    "data": [
+        'report/picking_templates.xml',
+    ],
+}
