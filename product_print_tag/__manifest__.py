b803c3d91e61679b90f8ab783680d938c9af268b
JaviZamoraDev <javier.zamora.dev@gmail.co>
2023-08-09 09:42:51 -0300

    [ODOO-6981][UPD][product_print_tag] - Fix size and other

--- product_print_tag/__manifest__.py
+++ product_print_tag/__manifest__.py
@@ -9,7 +9,7 @@
     "website": "http://odoo.calyx-cloud.com.ar/",
     "license": "AGPL-3",
     "category": "Stock",
-    "version": "13.0.1.1.0",
+    "version": "13.0.1.2.0",
     "depends": [
         "stock"
     ],
